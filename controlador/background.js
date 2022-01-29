716dad483f5028f41dae18b27bfa2d7e49e86e28
Piero <matematicadivina1@gmail.com>
2022-01-29 00:54:04 -0800

    no se puede crear un indexDB en el service worker

--- controlador/background.js
+++ controlador/background.js
@@ -5,8 +5,13 @@ import {notification} from "./modules/notifications.js"
 chrome.commands.onCommand.addListener(notification)
 
 chrome.action.onClicked.addListener(tab=>{
+    
+    (/^https:\/\/www.linkedin.com\/in\//).test(tab.url)
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: injectedFunction
+        function: (/^https:\/\/www.linkedin.com\/in\//).test(tab.url)? 
+            injectedFunction:
+            ()=>{alert('No estas en un perfil de linkedin para usar el scrapper')}
     })
+
 })
--- controlador/modules/info.js
+++ controlador/modules/info.js
@@ -1,8 +1,13 @@
+
+export let array;
 export function injectedFunction (){
     Array.from(document.getElementsByClassName("pv-profile-section__see-more-inline")).forEach(e=>e.click())
     let array = Array.from(document.getElementsByClassName("pv-entity__school-name"))
     let text = Array.from(document.getElementsByTagName("h1"))[0]
-    return [text.innerText, ...array.map(e=>e.innerText)]
+    console.log([text.innerText, ...array.map(e=>e.innerText)])
+
+    array = [text.innerText, ...array.map(e=>e.innerText)]
+    return array
 }
 
 
